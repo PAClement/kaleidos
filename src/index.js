4a6058f0e08e6dec72e4b0f6525ec1cdac12d35f
clement <clementpaquentin@hotmail.fr>
2024-05-04 01:01:28 +0200

    feat(index): add react router dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App';
+import Game from "./pages/Game";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App/>
+        <React.StrictMode>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="/game" element={<Game />} />
+                </Routes>
+            </Router>
+        </React.StrictMode>,
     </React.StrictMode>
 );
