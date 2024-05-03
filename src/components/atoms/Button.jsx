1413e9e20aeb4de91f7764fd04b1e5395d934d43
clement <clementpaquentin@hotmail.fr>
2024-05-04 01:01:40 +0200

    feat(atom): update button component

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -4,9 +4,8 @@ const Button = (props) => {
 
     return (
         <button type="button"
-                className={`text-white bg-${props.color}-700 hover:bg-${props.color}-800 focus:ring-4 focus:ring-${props.color}-300 font-medium
-                    rounded-lg text-xl px-10 py-5 me-2 mb-2 dark:bg-${props.color}-600 dark:hover:bg-${props.color}-700 focus:outline-none
-                    dark:focus:ring-${props.color}-800`}>
+                className={`text-white ${props.color} focus:ring-4 font-medium
+                    rounded-lg text-xl px-10 py-5 me-2 mb-2 focus:outline-none`}>
             {props.name}
         </button>
     );
