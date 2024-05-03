532d3469f7d88779f11c6280f2077a9f35a00aca
clement <clementpaquentin@hotmail.fr>
2024-05-04 00:44:56 +0200

    feat(app): update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Button from "./components/atoms/Button";
 
 function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div className={"grid h-screen place-items-center"}>
+            <div>
+                <h1 className={"text-6xl text-center font-bold mb-10"}>
+                    Welcome to le jeu
+                </h1>
+                <div className={"flex justify-center"}>
+                    <Button name={"Commencer une partie"} color={"indigo"}></Button>
+                </div>
+                <div className={"flex justify-center"}>
+                    <Button name={"Voir les règles"} color={"gray"}></Button>
+                </div>
+            </div>
+        </div>
     );
 }
 
