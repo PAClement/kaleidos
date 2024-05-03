47f5fa286dc98e0699e273758e0b579a6a0ea6a7
clement <clementpaquentin@hotmail.fr>
2024-05-04 01:01:55 +0200

    feat(app): add link to page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Link } from 'react-router-dom'
 import Button from "./components/atoms/Button";
 
 function App() {
@@ -9,10 +10,12 @@ function App() {
                     Welcome to le jeu
                 </h1>
                 <div className={"flex justify-center"}>
-                    <Button name={"Commencer une partie"} color={"indigo"}></Button>
+                    <Link to="/game">
+                        <Button name={"Commencer une partie"} color={"bg-indigo-700 hover:bg-indigo-800 focus:ring-indigo-300"}></Button>
+                    </Link>
                 </div>
                 <div className={"flex justify-center"}>
-                    <Button name={"Voir les règles"} color={"gray"}></Button>
+                    <Button name={"Voir les règles"} color={"bg-gray-700 hover:bg-gray-800 focus:ring-gray-300"}></Button>
                 </div>
             </div>
         </div>
