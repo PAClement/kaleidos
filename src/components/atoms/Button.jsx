b7c9fe7fb067297a65631b79f2942a1f40240c07
clement <clementpaquentin@hotmail.fr>
2024-05-06 01:01:26 +0200

    feat(component): add type custom

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = (props) => {
 
     return (
-        <button type="button"
+        <button type={props.type}
                 className={`text-white ${props.color} focus:ring-4 font-medium
                     rounded-lg text-xl px-10 py-5 me-2 mb-2 focus:outline-none`}>
             {props.name}
