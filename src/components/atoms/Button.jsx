ad17b6333857b8ba4bd37ef4439cea6f09ee8d5d
PAClement <clementpaquentin@hotmail.fr>
2024-05-24 15:10:29 +0200

    feat(atom): add onClick parameters

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -4,6 +4,7 @@ const Button = (props) => {
 
     return (
         <button type={props.type}
+                onClick={props.onClick}
                 className={`text-white ${props.color} focus:ring-4 font-medium
                     rounded-lg text-xl px-10 py-5 me-2 mb-2 focus:outline-none`}>
             {props.name}
