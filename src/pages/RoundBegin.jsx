26857949d510293d7d1297a91ed87aad8ca1dce3
clement <clementpaquentin@hotmail.fr>
2024-10-30 00:49:54 +0100

    refactor(page): add currentRound to text begin

--- src/pages/RoundBegin.jsx
+++ src/pages/RoundBegin.jsx
@@ -14,7 +14,7 @@ const RoundBegin = (props) => {
 
         switch (timeLeft) {
             case 5:
-                setDisplayValueTime('Le round va commencer !');
+                setDisplayValueTime(`La manche ${props.currentRound} va commencer !`);
                 break;
             case 4:
                 setDisplayValueTime('3');
@@ -37,7 +37,7 @@ const RoundBegin = (props) => {
 
     return (
         <div className={'grid h-screen place-items-center'}>
-            <p className={'text-center text-9xl'}>{displayValueTime}</p>
+            <p className={'text-center text-6xl'}>{displayValueTime}</p>
             <div className={'flex flex-col gap-5 items-center'}>
                 <span className={'text-4xl'}>Lettre </span>
                 <span className={'text-4xl'}>{props.letter}</span>
